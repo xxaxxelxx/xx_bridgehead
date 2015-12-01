6348b9c4d366f857ad3401d2e7689270232f1932
xxaxxelxx <x@axxel.net>
2015-12-01 11:57:37 +0100

    updated

--- dockercleanup.sh
+++ dockercleanup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 docker stop $(docker ps -q) 2>/dev/null
+docker rm $(docker ps -aq) 2>/dev/null
 ps waux | grep icecast_trigger.sh | grep -v grep | awk '{print $2}' | xargs kill -9 
 exit
