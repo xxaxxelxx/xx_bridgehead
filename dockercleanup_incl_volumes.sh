5e10f75a9ade9fd336e061d78066dbbe35c17473
xxaxxelxx <x@axxel.net>
2015-12-02 10:41:18 +0100

    updated

--- dockercleanup_incl_volumes.sh
+++ dockercleanup_incl_volumes.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 CONTAINERLIST="$(docker ps -q)"
 docker stop $CONTAINERLIST 2>/dev/null
-docker rm $CONTAINERLIST 2>/dev/null
+docker rm $(docker ps -aq) 2>/dev/null
 ps waux | grep icecast_trigger.sh | grep -v grep | awk '{print $2}' | xargs kill -9 
 exit
