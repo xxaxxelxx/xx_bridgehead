4f9b229d6ed39edcb1d2512deba9abb15acf5ca1
xxaxxelxx <x@axxel.net>
2015-12-08 15:59:16 +0100

    updated

--- iptables.add_player_ip_to_proxy.sh
+++ iptables.add_player_ip_to_proxy.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 LIST_PORT="8000"
 for PORT in $LIST_PORT; do
-    for IP in $LIST_IP; do
-	iptables -I DOCKER -i eth0 -s $1 -p tcp --dport $PORT -j ACCEPT
-	iptables -I DOCKER -i eth0 -s $1 -p udp --dport $PORT -j ACCEPT
-    done
+	iptables -v -I DOCKER -i eth0 -s $1 -p tcp --dport $PORT -j ACCEPT
+	iptables -v -I DOCKER -i eth0 -s $1 -p udp --dport $PORT -j ACCEPT
 done
 echo "Do not forget to copy file iptables.addresses.players to /etc"
 exit
