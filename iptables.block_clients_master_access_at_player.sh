42b5eff545a0a760647133c2fafa4cefe812cea5
xxaxxelxx <x@axxel.net>
2015-12-09 10:46:47 +0100

    updated

--- iptables.block_clients_master_access_at_player.sh
+++ iptables.block_clients_master_access_at_player.sh
@@ -6,4 +6,5 @@ for PORT in $LIST_PORT; do
     DOCKERNET="$(iptables -L DOCKER -n | grep $PORT | awk '{print $5}' | head -n 1 | sed 's|\..*||').0.0.0/8"
 echo $DOCKERNET
 #    iptables -I DOCKER -i eth0 -p tcp --dport $PORT ! -s $DOCKERNET -m string --algo bm --string "_master" -j DROP 
+done
 exit
