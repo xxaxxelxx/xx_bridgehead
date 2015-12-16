32de64ff5d10943e42030c91bf6b49aec07782eb
xxaxxelxx <x@axxel.net>
2015-12-16 12:31:55 +0100

    updated

--- watchdog.sh
+++ watchdog.sh
@@ -7,18 +7,21 @@ LIMIT=95
 
 CNT=0
 while true; do
-    IDLE=$(echo "scale=0; ($(grep "cpu " /proc/stat | awk -F ' ' '{total = $2 + $3 + $4 + $5} END {print $5*100/total}')/3)*3" | bc) #"
-    if [ $IDLE -gt $LIMIT ]; then 
-	ALERTED=0
-	CNT=$(($CNT+1))
-	if [ $CNT -ge $EJECT_AT ]; then
-	    echo "eject!"
-	    reboot
-	    exit
+    ps waux | grep icecast_player > /dev/null
+    if [ $? -eq 0 ]; then
+	IDLE=$(echo "scale=0; ($(grep "cpu " /proc/stat | awk -F ' ' '{total = $2 + $3 + $4 + $5} END {print $5*100/total}')/3)*3" | bc) #"
+	if [ $IDLE -gt $LIMIT ]; then 
+	    ALERTED=0
+	    CNT=$(($CNT+1))
+	    if [ $CNT -ge $EJECT_AT ]; then
+		echo "eject!"
+		reboot
+		exit
+	    fi
+	else
+	    ALERTED=1
+	    CNT=0
 	fi
-    else
-	ALERTED=1
-	CNT=0
     fi
 sleep $SLEEP
 done
