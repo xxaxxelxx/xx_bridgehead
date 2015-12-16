823a7d90e383f7af2d7d1acd9218291ace1d0b4c
xxaxxelxx <x@axxel.net>
2015-12-16 13:01:14 +0100

    updated

--- iptables.block_clients_master_access_at_player.sh
+++ iptables.block_clients_master_access_at_player.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 LIST_PORT="8000"
 for PORT in $LIST_PORT; do
-    DOCKERNET="$(iptables -L DOCKER -n | grep ACCEPT | grep $PORT | awk '{print $5}' | head -n 1 | sed 's|\..*||').0.0.0/8"
-    iptables -v -I DOCKER -i eth0 -p tcp --dport $PORT ! -s $DOCKERNET -m string --algo bm --string "_master" -j DROP 
+    DETECTED_FIRST_IP="$(iptables -L DOCKER -n | grep ACCEPT | grep $PORT | awk '{print $5}' | head -n 1 | sed 's|\..*||')"
+    if [ "x$DETECTED_FIRST_IP" != "x" ]; then
+	DOCKERNET="$DETECTED_FIRST_IP.0.0.0/8"
+	iptables -v -I DOCKER -i eth0 -p tcp --dport $PORT ! -s $DOCKERNET -m string --algo bm --string "_master" -j DROP 
+    fi
 done
 exit
