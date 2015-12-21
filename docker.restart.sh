ac4a9f56830ff74e332f7192cbea120879f3675f
xxaxxelxx <x@axxel.net>
2015-12-21 13:32:22 +0100

    updated

--- docker.restart.sh
+++ docker.restart.sh
@@ -2,11 +2,25 @@
 ID=$1
 test "x$ID" == "x"  && exit 1
 
-STOPLIST=($(docker ps | grep '^$ID'))
-
+echo "STOPPING CONTAINERS"
+STOPLIST=($(docker ps | grep $ID | sed -e 's/\ *$//' | sed 's/.* //'))
 for ELEM in ${STOPLIST[@]}; do
-    echo $ELEM
+    docker stop $ELEM
 done
 
+echo "REMOVING CONTAINERS"
+RMLIST=($(docker ps -a | grep $ID | sed -e 's/\ *$//' | sed 's/.* //'))
+IMAGELIST=($(docker ps -a | grep $ID | awk '{print $2}'))
+for ELEM in ${STOPLIST[@]}; do
+    docker rm $ELEM
+done
+
+echo "PULLING IMAGES"
+for ELEM in ${IMAGELIST[@]}; do
+    docker pull $ELEM
+done
+
+echo "STARTING CONTAINERS"
+bash RUN/${ID}*
 
 exit
