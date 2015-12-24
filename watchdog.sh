bc1e0ec3f34c4e00f38cf716079412289535af67
xxaxxelxx <x@axxel.net>
2015-12-24 09:05:08 +0100

    updated

--- watchdog.sh
+++ watchdog.sh
@@ -6,15 +6,21 @@ EJECT_AT=10
 LIMIT=95
 
 CNT=0
+
+STATFILE=/tmp/watchdog.info
+
+echo "$(date) | Started." > $STATFILE
+
 while true; do
     ps waux | grep icecast_player > /dev/null
     if [ $? -eq 0 ]; then
 	IDLE=$(echo "scale=0; ($(grep "cpu " /proc/stat | awk -F ' ' '{total = $2 + $3 + $4 + $5} END {print $5*100/total}')/3)*3" | bc) #"
 	if [ $IDLE -gt $LIMIT ]; then 
 	    ALERTED=0
+	    echo "$(date) | Alert: $IDLE idle. ${CNT}#${EJECT_AT}" > $STATFILE
 	    CNT=$(($CNT+1))
 	    if [ $CNT -ge $EJECT_AT ]; then
-		echo "eject!"
+		echo "$(date) | EJECT." > $STATFILE
 		reboot
 		exit
 	    fi
