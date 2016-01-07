9d6bca1220dd6dff7299e8e2187f5177ad9832a2
xxaxxelxx <x@axxel.net>
2016-01-07 16:26:51 +0100

    updated

--- docker.restart.sh
+++ docker.restart.sh
@@ -1,28 +1,31 @@
 #!/bin/bash
-ID=$1
-test "x$ID" == "x"  && exit 1
 
-echo "STOPPING CONTAINERS"
-STOPLIST=($(docker ps | grep $ID | sed -e 's/\ *$//' | sed 's/.* //'))
-for ELEM in ${STOPLIST[@]}; do
-    docker stop $ELEM
-done
+while [ test "x$1" != "x" ] ; do
+    ID=$1
 
-echo "REMOVING CONTAINERS"
-RMLIST=($(docker ps -a | grep $ID | sed -e 's/\ *$//' | sed 's/.* //'))
-IMAGELIST=($(docker ps -a | grep $ID | awk '{print $2}' | sort -u))
-for ELEM in ${STOPLIST[@]}; do
-    docker rm $ELEM
-done
+    echo "STOPPING CONTAINERS"
+    STOPLIST=($(docker ps | grep $ID | sed -e 's/\ *$//' | sed 's/.* //'))
+    for ELEM in ${STOPLIST[@]}; do
+	docker stop $ELEM
+    done
 
-echo "PULLING IMAGES"
-for ELEM in ${IMAGELIST[@]}; do
-    docker pull $ELEM
-done
+    echo "REMOVING CONTAINERS"
+    RMLIST=($(docker ps -a | grep $ID | sed -e 's/\ *$//' | sed 's/.* //'))
+    IMAGELIST=($(docker ps -a | grep $ID | awk '{print $2}' | sort -u))
+    for ELEM in ${STOPLIST[@]}; do
+	docker rm $ELEM
+    done
 
-echo "STARTING CONTAINERS"
-for FILE in RUN/${ID}*; do
-    cat $FILE | bash
+    echo "PULLING IMAGES"
+    for ELEM in ${IMAGELIST[@]}; do
+	docker pull $ELEM
+    done
+
+    echo "STARTING CONTAINERS"
+    for FILE in RUN/${ID}*; do
+	cat $FILE | bash
+    done
+    shift
 done
 
 exit
