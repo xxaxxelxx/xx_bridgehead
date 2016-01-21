a19d0580da5d82365d5778dc6273a8f02de35bb9
xxaxxelxx <x@axxel.net>
2016-01-21 10:52:25 +0100

    updated

--- reflectdog.sh
+++ reflectdog.sh
@@ -10,6 +10,26 @@ LOGFILE=/root/reflect.log
 REFCNT=0
 ACCCNT=0
 STATUS="ACCEPT"
+
+FORCE="$1"
+
+
+if [ "x$FORCE" == "xR" ]; then
+    test -r /root/loadbalancer.addr
+    if [ $? -eq 0 ]; then
+	LOADBALANCER=$(cat /root/loadbalancer.addr)
+	docker stop reflector; docker rm reflector
+	docker run -d --name reflector -e TARGET_SERVER=$LOADBALANCER -e TARGET_PORT=80 -p 80:80 --restart=always xxaxxelxx/xx_reflector
+	iptables -t nat -I DOCKER 1 -p tcp --dport 8000 -j DNAT --to-destination $(iptables -n -L DOCKER | grep -w dpt:80 | awk '{print $5}'):80
+	exit
+    fi
+elif [ "x$FORCE" == "xA" ]; then
+    iptables -t nat -D DOCKER $(iptables -t nat -L DOCKER --line-numbers | grep -w dpt:8000 | grep ':80$' | awk '{print $1}')
+    docker stop reflector; docker rm reflector
+    docker run -d --name reflector -e TARGET_SERVER=%0 -e TARGET_PORT=8000 -p 80:80 --restart=always xxaxxelxx/xx_reflector
+    exit
+fi
+
 while true; do
 # LOGGING #
     test -r $LOGFILE
@@ -34,6 +54,7 @@ while true; do
 			LOADBALANCER=$(cat /root/loadbalancer.addr)
 			docker stop reflector; docker rm reflector
 			docker run -d --name reflector -e TARGET_SERVER=$LOADBALANCER -e TARGET_PORT=80 -p 80:80 --restart=always xxaxxelxx/xx_reflector && \
+			iptables -t nat -I DOCKER 1 -p tcp --dport 8000 -j DNAT --to-destination $(iptables -n -L DOCKER | grep -w dpt:80 | awk '{print $5}'):80
 			STATUS="REFLECT"
 			ACCCNT=0
 		    fi
@@ -51,6 +72,7 @@ while true; do
 		    echo "X X X X X X" >> $LOGFILE
 
 		    if [ $? -eq 0 ]; then
+			iptables -t nat -D DOCKER $(iptables -t nat -L DOCKER --line-numbers | grep -w dpt:8000 | grep ':80$' | awk '{print $1}')
 			docker stop reflector; docker rm reflector
 			docker run -d --name reflector -e TARGET_SERVER=%0 -e TARGET_PORT=8000 -p 80:80 --restart=always xxaxxelxx/xx_reflector && \
 			STATUS="ACCEPT"
