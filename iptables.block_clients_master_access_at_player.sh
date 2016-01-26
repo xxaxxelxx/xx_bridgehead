1483f26cc427a65961b1293e9be012e98a9b9d9b
xxaxxelxx <x@axxel.net>
2016-01-26 13:06:09 +0100

    updated

--- iptables.block_clients_master_access_at_player.sh
+++ iptables.block_clients_master_access_at_player.sh
@@ -2,8 +2,6 @@
 LIST_PORT="8000"
 DOCKER_NET="172.17.0.0/16"
 for PORT in $LIST_PORT; do
-    if [ "x$DETECTED_FIRST_IP" != "x" ]; then
-	iptables -v -I DOCKER -i eth0 -p tcp --dport $PORT ! -s $DOCKER_NET -m string --algo bm --string "_master" -j DROP 
-    fi
+    iptables -v -I DOCKER -i eth0 -p tcp --dport $PORT ! -s $DOCKER_NET -m string --algo bm --string "_master" -j DROP 
 done
 exit
--- iptables.block_dos_at_player.sh
+++ iptables.block_dos_at_player.sh
@@ -2,10 +2,8 @@
 LIST_PORT="8000"
 DOCKER_NET="172.17.0.0/16"
 for PORT in $LIST_PORT; do
-    if [ "x$DETECTED_FIRST_IP" != "x" ]; then
-	iptables -v -I DOCKER -p tcp --dport $PORT ! -s $DOCKER_NET -m state --state NEW -m recent -i eth0 --set --name BLOCKDOS -j ACCEPT
-	iptables -v -I DOCKER -p tcp --dport $PORT ! -s $DOCKER_NET -m recent -i eth0 --update --seconds 60 --hitcount 10 --rttl --name BLOCKDOS -j DROP
-    fi
+    iptables -v -I DOCKER -p tcp --dport $PORT ! -s $DOCKER_NET -m state --state NEW -m recent -i eth0 --set --name BLOCKDOS -j ACCEPT
+    iptables -v -I DOCKER -p tcp --dport $PORT ! -s $DOCKER_NET -m recent -i eth0 --update --seconds 60 --hitcount 10 --rttl --name BLOCKDOS -j DROP
 done
 exit
 
