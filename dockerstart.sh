7cc70ab0dd9e57eb7549eac5a901baddc9b877af
xxaxxelxx <x@axxel.net>
2016-03-08 12:20:52 +0100

    updated

--- dockerstart.sh
+++ dockerstart.sh
@@ -163,6 +163,13 @@ elif [ $MODE = "LOADBALANCER" ]; then
 	$DOCKER_CMD && rm -f "$RUNDIR/${DOCKER_NAME}."* && echo "$DOCKER_CMD" >> $RUNDIR/$DOCKER_NAME.$(date +%Y-%m-%d_%H%M%S)
     done
 
+    for CUSTOMER in ${A_CUSTOMERS[@]}; do
+	# RUN GEOGRAPH CUSTOMERS
+	if [ "x$CUSTOMER" == "xadmin" ]; then continue; fi
+	DOCKER_NAME="introlyzer_$CUSTOMER" && DOCKER_CMD="docker run -d --name $DOCKER_NAME --volumes-from customerweb --restart=always xxaxxelxx/xx_introlyzer $CUSTOMER"
+	$DOCKER_CMD && rm -f "$RUNDIR/${DOCKER_NAME}."* && echo "$DOCKER_CMD" >> $RUNDIR/$DOCKER_NAME.$(date +%Y-%m-%d_%H%M%S)
+    done
+
 #    for CUSTOMER in ${A_CUSTOMERS[@]}; do
 #	# RUN ACCOUNT CUSTOMERS
 #	CUSTOMER_PRICE="$(dialog --stdout --inputbox "Set ${CUSTOMER}'s price in Euro per GByte please. Use the following format:0#0.06|10000#0.05|20000#0.03" $HEIGHT $WIDTH 0#0.06|10000#0.054|25000#0.046|50000#0.035|100000#0.026|250000#0.018|500000#0.012)"
